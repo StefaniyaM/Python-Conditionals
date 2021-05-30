978cf415e883c48ddec915d022a909768f56e1c6
Stefaniya <>
2021-05-30 21:23:18 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,90 @@
+# x = 15
+# y = 7
+
+# if x > y:
+#    print('yes')
+
+
+
+  #  =====================
+
+# x = 5
+# y = 7
+
+# if x < y:
+#   print('No')
+
+  #  ====================
+
+# if 'Hello' in 'Hello world!':
+#       print('Yes')
+
+  #  ====================
+
+x = 5
+y = 7
+
+# if x > y:
+#    print('yes it is greater')
+# elif x < y:
+#    print('no it is less')
+# else:
+#    print('Neither')   
+
+  #  ====================
+
+
+'''
+Nested if  Statements
+'''
+
+# num = 0
+
+# if num >= 0:
+#     if num == 0:
+#        print("Zero")
+#     else:
+#        print("Positive number")
+# else:
+#   print("Negative number")
+
+ #  ====================
+
+'''
+one-line if statement
+'''
+
+# if 7 > 5: print('yes it is')
+
+# ========================
+
+'''
+Ternary operator
+'''
+
+# print('Yes') if 7 > 5 else print('No')
+
+# #  it is the same as:
+
+# if 7 > 5:
+#    print('Yes')
+# else:
+#    print('No')
+
+# ========================
+
+# if 100 > 50:
+#    print('Yeah')
+
+
+
+# cifra = 4.5
+
+# if cifra <= 2:
+#      if cifra == 2:
+#        print('two')
+#      else:
+#        print('Incorrect')
+# else: 
+#    print('Correct')
+
